b1b3e2c3b99db099af859471ba61b68db589cfb7
Konrad Wenc <konrad.wenc@outlook.com>
2021-04-03 00:18:14 +0200

    Initial project with localizatioin

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,57 @@
 import Head from 'next/head'
+import Link from "next/link"
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+import Hero from "../components/hero";
+import {useRouter} from "next/router";
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+export default function Home(props) {
+    const router = useRouter()
+    const {locale, locales, defaultLocale} = router
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Funcoldes</title>
+                <Link href="/mision" locale="en">
+                    <a>To another</a>
+                </Link>
+            </Head>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+            <main className={styles.main}>
+                <Hero/>
+                <p>current locale: {locale}</p>
+                <p>Default locale: {defaultLocale}</p>
+                <p>Configured locales: {JSON.stringify(locales)}</p>
+                <p>Somos una Organización sin ánimo de lucro, de nacionalidad Colombiana, constituida el 25 de Marzo de
+                    2004,
+                    en Bogotá.</p>
+                <p>El objetivo y la trascendencia de la entidad ha sido en el acompañamiento de entidades Privadas,
+                    Públicas y
+                    Organismos Internacionales, en programas de capacitación, Informal, formación científica y
+                    pedagógica dentro de
+                    las necesidades de desarrollo de las entidades, comunidades, construyendo procesos que han permitido
+                    procesos
+                    investigativos en lo social, económico y político, permitiendo la generación de soluciones reales a
+                    las
+                    necesidades puntuales de la sociedad colombiana.</p>
+                <p>La experiencia de la entidad es amplia en el campo privado y público. Siempre comprometido con las
+                    necesidades
+                    y aporte a la sociedad, generando desde su seno proyectos y procesos que han permitido beneficiar a
+                    muchos
+                    colombianos.fdsfd</p>
+            </main>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            <footer className={styles.footer}>
+                <a
+                    href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Powered by{' '}
+                    <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo}/>
+                </a>
+            </footer>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+    )
 }
